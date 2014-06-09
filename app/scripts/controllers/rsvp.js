b7faad832c32a395f95713656ef8de5f47f5346b
JD Corr <jd@jdcorr.com>
2014-06-08 22:01:48 -0700

    rsvp: adding additional states

--- app/scripts/controllers/rsvp.js
+++ app/scripts/controllers/rsvp.js
@@ -3,14 +3,32 @@
 angular.module('jandkApp')
 .controller('RsvpCtrl', function ($scope, $http) {
 
+	$scope.step1 = 'step1';
+	$scope.step2 = 'step2';
+	$scope.step3 = 'step3';
+	$scope.completion = 'completion';
+	$scope.state = $scope.step1;
+
 	$scope.rsvpCode = '';
 	$scope.continueDisabled = true;
 	$scope.continueBtn = angular.element(document.querySelector('.continue-btn'));
 
-	$scope.retrieveRsvp = function() {
+	$scope.getRsvp = function() {
+		$scope.state = $scope.step2;
+
+		console.log($scope.rsvpCode);
+
 		$http.get('/api/rsvps/'+$scope.rsvpCode).success(function(rsvp) {
 			$scope.rsvp = rsvp;
 		});
+	};
+
+	$scope.showPreSubmit = function() {
+		$scope.state = $scope.step3;
+	};
+
+	$scope.backToInfo = function() {
+		$scope.state = $scope.step2;
 	};
 
 	$scope.checkForRsvpCode = function() {
