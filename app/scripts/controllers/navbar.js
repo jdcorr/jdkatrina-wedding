529ebb6a365a3fc379b7fc1300d47f3c31104b4e
JD Corr <jd@jdcorr.com>
2014-07-08 00:41:27 -0700

    navbar: adding smooth scroll behavior

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -37,10 +37,65 @@ angular.module('jandkApp')
 		},
 	];
 
-	$scope.gotoAnchor = function (anchor) {
+	$scope.gotoAnchor = function (elementId) {
 		// set the location.hash to the id of the element you wish to scroll to.
-		$location.hash(anchor);
-		$anchorScroll();
+		//$location.hash(anchor);
+		//$anchorScroll();
+
+		//$anchorSmoothScroll.scrollTo(anchor);
+				
+		var i;
+		var startY = currentYPosition();
+		var stopY = elmYPosition(elementId) - 100;
+		var distance = stopY > startY ? stopY - startY : startY - stopY;
+		if (distance < 100) {
+			scrollTo(0, stopY); return;
+		}
+		var speed = Math.round(distance / 2);
+		if (speed >= 20) speed = 20;
+		var step = Math.round(distance / 25);
+		var leapY = stopY > startY ? startY + step : startY - step;
+		var timer = 0;
+		if (stopY > startY) {
+			for (i = startY; i < stopY; i += step) {
+				setTimeout('window.scrollTo(0, '+leapY+')', timer * speed);
+				leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+			} return;
+		}
+		for (i = startY; i > stopY; i -= step) {
+			setTimeout('window.scrollTo(0, '+leapY+')', timer * speed);
+			leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+		}
+	
+		/* currentYPosition -
+		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+		function currentYPosition() {
+			// Firefox, Chrome, Opera, Safari
+			if (window.pageYOffset) {
+				return window.pageYOffset;
+			}
+			// Internet Explorer 6 - standards mode
+			if (document.documentElement && document.documentElement.scrollTop) {
+				return document.documentElement.scrollTop;
+			}
+			// Internet Explorer 6, 7 and 8
+			if (document.body.scrollTop) {
+				return document.body.scrollTop;
+			}
+			return 0;
+		}
+
+		/* scrollTo -
+		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+		function elmYPosition(elementId) {
+			var elm = document.getElementById(elementId);
+			var y = elm.offsetTop;
+			var node = elm;
+			while (node.offsetParent && node.offsetParent != document.body) {
+				node = node.offsetParent;
+				y += node.offsetTop;
+			} return y;
+		}
 	};
 
 	$scope.isActive = function(route) {
