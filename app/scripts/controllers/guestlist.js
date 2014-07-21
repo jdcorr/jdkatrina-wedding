d6150ebcb79e5ba567a03fceb5da9b9430266243
JD Corr <jd@jdcorr.com>
2014-07-20 23:34:09 -0700

    adding tally for guestlist page

--- app/scripts/controllers/guestlist.js
+++ app/scripts/controllers/guestlist.js
@@ -5,10 +5,19 @@ angular.module('jandkApp')
 
 	$scope.confirmedRsvps = [];
 	$scope.declinedRsvps = [];
+	$scope.totalConfirmedAdults = 0;
+	$scope.totalConfirmedChildren = 0;
+	$scope.totalConfirmedInfants = 0;
 
 	$scope.getConfirmedRsvps = function() {
 		$http.get('/api/rsvps/confirmed').success(function(rsvps) {
 			$scope.confirmedRsvps = rsvps;
+
+			for (var i=0; i<rsvps.length; i++) {
+				$scope.totalConfirmedAdults += $scope.confirmedRsvps[i].totalAdults;
+				$scope.totalConfirmedChildren += $scope.confirmedRsvps[i].totalChildren;
+				$scope.totalConfirmedInfants += $scope.confirmedRsvps[i].totalInfants;
+			} 
 		});
 	};
 
